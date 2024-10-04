99cdb4eb86dcae36ce343e813b494fa7a5a9a22c
LessiaSa <0162221@mail.ru>
2024-10-04 14:58:24 +0700

    NewFile

--- main.py
+++ main.py
@@ -29,35 +29,57 @@
 #
 # create_merget_file('txt', 'merget_file')
 
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    line_1 = {}
-    count_1 = 0
-    for line in file_1.readlines():
-        count_1 += 1
-        line_1['1.txt'] = count_1
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    text_1 = file_1.read()
+# with open('1.txt', 'r', encoding='utf-8') as file_1:
+#     line_1 = {}
+#     count_1 = 0
+#     for line in file_1.readlines():
+#         count_1 += 1
+#         line_1['1.txt'] = count_1
+# with open('1.txt', 'r', encoding='utf-8') as file_1:
+#     text_1 = file_1.read()
+#
+# with open('2.txt', 'r', encoding='utf-8') as file_2:
+#     line_2 = {}
+#     count_2 = 0
+#     for line in file_2.readlines():
+#         count_2 += 1
+#         line_2['2.txt'] = count_2
+# with open('2.txt', 'r', encoding='utf-8') as file_2:
+#     text_2 = file_2.read()
+#
+# with open('3.txt', 'r', encoding='utf-8') as file_3:
+#     line_3 = {}
+#     count_3 = 0
+#     for line in file_3.readlines():
+#         count_3 += 1
+#         line_3['3.txt'] = count_3
+# with open('3.txt', 'r', encoding='utf-8') as file_3:
+#     text_3 = file_3.read()
+#
+# join = sorted(list(line_1.items()) + list(line_2.items()) + list(line_3.items()), key=lambda x: x[1])
+#
+# with open('result.txt', 'w', encoding='utf-8') as file_result:
+#     for line in join:
+#         file_result.write(f'{join[0][0]}\n {join[0][1]}\n {text_2}\n {join[1][0]}\n {join[1][1]}\n {text_1}\n{join[2][0]}\n{join[2][1]}\n {text_3}\n')
 
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    line_2 = {}
-    count_2 = 0
-    for line in file_2.readlines():
-        count_2 += 1
-        line_2['2.txt'] = count_2
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    text_2 = file_2.read()
+import os
 
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    line_3 = {}
-    count_3 = 0
-    for line in file_3.readlines():
-        count_3 += 1
-        line_3['3.txt'] = count_3
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    text_3 = file_3.read()
 
-join = sorted(list(line_1.items()) + list(line_2.items()) + list(line_3.items()), key=lambda x: x[1])
+def sort_files():
+    files = os.listdir('sorted')
+    result = []
+    for file_ in files:
+        with open(f'sorted/{file_}', 'r', encoding='utf8') as f:
+            text = f.read()
+            f.seek(0)
+            result.append({'name': file_, 'count_lines': len(f.readlines()), 'text': text})
 
-with open('result.txt', 'w', encoding='utf-8') as file_result:
-    for line in join:
-        file_result.write(f'{join[0][0]}\n {join[0][1]}\n {text_2}\n {join[1][0]}\n {join[1][1]}\n {text_1}\n{join[2][0]}\n{join[2][1]}\n {text_3}\n')
+    sorted_files = sorted(result, key=lambda f: f['count_lines'])
+    with open('result.txt', 'w', encoding='utf8') as f:
+        for file_ in sorted_files:
+            f.write(file_['name']+'\n')
+            f.write(str(file_['count_lines'])+'\n')
+            f.write(file_['text']+'\n')
+
+
+sort_files()
